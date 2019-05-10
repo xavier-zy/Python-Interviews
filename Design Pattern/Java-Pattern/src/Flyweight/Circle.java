493d827d2e6933d40685dfcddee5c7f10c300c08
xavier_zy <mid_earth123@outlook.com>
2019-05-10 23:53:33 +0800

    Flyweight Pattern

---
+++ Design Pattern/Java-Pattern/src/Flyweight/Circle.java
@@ -0,0 +1,28 @@
+package Flyweight;
+
+public class Circle implements Shape {
+    private int x, y, radius;
+    private String color;
+
+    public Circle(String color) {
+        this.color = color;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Circle: Draw() [Color : " + color
+                + ", x : " + x + ", y :" + y + ", radius :" + radius);
+    }
+}
