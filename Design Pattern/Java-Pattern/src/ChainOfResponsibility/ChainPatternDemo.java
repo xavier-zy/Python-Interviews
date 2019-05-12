65a1dbb90a0667dd084149eb6fe7303fc499d2f6
xavier_zy <mid_earth123@outlook.com>
2019-05-12 10:33:56 +0800

    Chain of Responsibility Pattern

---
+++ Design Pattern/Java-Pattern/src/ChainOfResponsibility/ChainPatternDemo.java
@@ -0,0 +1,30 @@
+package ChainOfResponsibility;
+
+public class ChainPatternDemo {
+    private static AbstractLogger getChainOfLoggers() {
+
+        AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+        AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+        AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+        errorLogger.setNextLogger(fileLogger);
+        fileLogger.setNextLogger(consoleLogger);
+
+        return errorLogger;
+    }
+
+    public static void main(String[] args) {
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        System.out.println("INFO");
+        loggerChain.logMessage(AbstractLogger.INFO, "This is an information.\n");
+
+        System.out.println("DEBUG");
+        loggerChain.logMessage(AbstractLogger.DEBUG,
+                "This is a debug level information.\n");
+
+        System.out.println("ERROR");
+        loggerChain.logMessage(AbstractLogger.ERROR,
+                "This is an error information.\n");
+    }
+}
